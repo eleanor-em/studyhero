57e430c89396c2c421f8a7c1cb5a4604fdea00cf
Eleanor McMurtry <elmcmurtry1@gmail.com>
2016-11-13 02:35:58 +1100

    changed to plural

--- main/urls.py
+++ main/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     url(r'^new-subject/$', views.new_subject, name='new-subject'),
     url(r'^create-cards/$', views.create_cards, name='create-cards'),
     url(r'^delete-subject/$', views.delete_subject, name="delete-subject"),
-    url(r'^rest/card/$', views.rest_card, name="rest-card"),
+    url(r'^rest/cards/$', views.rest_card, name="rest-card"),
 ]
