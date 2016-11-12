a097a4cf49c4bf81bc4821046ca47cb8d6c02c01
Eleanor McMurtry <elmcmurtry1@gmail.com>
2016-11-12 11:57:33 +1100

    models and templates set up

--- main/models.py
+++ main/models.py
@@ -1,5 +1,65 @@
 from __future__ import unicode_literals
 
 from django.db import models
+import ast
 
-# Create your models here.
+class ListField(models.TextField):
+    # __metaclass__ = models.SubfieldBase
+    description = "Stores a python list"
+
+    def __init__(self, *args, **kwargs):
+        super(ListField, self).__init__(*args, **kwargs)
+
+    def to_python(self, value):
+        if not value:
+            value = []
+
+        if isinstance(value, list):
+            return value
+
+        return ast.literal_eval(value)
+        
+    def from_db_value(self, value, expression, connection, context):
+        if value is None:
+            return value;
+
+        return ast.literal_eval(value)
+
+    def get_prep_value(self, value):
+        if value is None:
+            return value
+
+        return unicode(value)
+
+    def value_to_string(self, obj):
+        value = self._get_val_from_obj(obj)
+        return self.get_db_prep_value(value)
+
+class Subject(models.Model):
+    RED = "#FFB5B9"
+    ORANGE = "#FFE6BF"
+    YELLOW = "#FFEDB5"
+    GREEN = "#B5F2BA"
+    BLUE = "#BFDAFF"
+    INDIGO = "#DABFFF"
+    VIOLET = "#FBBFFF"
+    
+    COLOURS = (
+        (RED, "Red"),
+        (ORANGE, "Orange"),
+        (YELLOW, "Yellow"),
+        (GREEN, "Green"),
+        (BLUE, "Blue"),
+        (INDIGO, "Indigo"),
+        (VIOLET, "Violet")
+    )
+
+    name = models.CharField(max_length=50, unique=True)
+    colour = models.CharField(max_length=7, choices=COLOURS, unique=True)
+    days = ListField()
+
+class Card(models.Model):
+    title = models.CharField(max_length=128, unique=True)
+    subject = models.ForeignKey(Subject)
+    points = models.IntegerField(default=1)
+    date = models.DateField()
